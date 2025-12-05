4b5097d9e3eb32fac4ed67360ec3e94b2fdb003b
bitm <1759743261@qq.com>
2025-12-06 05:35:31 +0800

    创建：自定义 WPF 控件库项目（ETool.WPF）

---
+++ ETool.WPF.App/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace ETool.WPF.App
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
